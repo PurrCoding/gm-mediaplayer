67afabf88da0302a76f883e1cd74809d59be9fc2
Shadowsun™ <12494967+CattoGamer@users.noreply.github.com>
2025-12-30 15:21:54 +0100

    Archive links should not run through resource services!
    When an archive link is requested with file extensions such as mp4, it is requested by the resource service, which is not correct!

--- lua/mediaplayer/services/resource/shared.lua
+++ lua/mediaplayer/services/resource/shared.lua
@@ -5,18 +5,37 @@ SERVICE.Abstract = true
 
 SERVICE.FileExtensions = {}
 
-function SERVICE:Match( url )
-	-- check supported file extensions
-	local ext = string.GetExtensionFromFilename(url)
-	if ext and ext:match("(.[^?]+)") then
-		ext = ext:match("(.[^?]+)")
+local urllib = url
 
-		for _, ext2 in pairs(self.FileExtensions) do
-			if ext == ext2 then
-				return true
+local function parseURL(url)
+	local success, urlinfo = pcall(urllib.parse2, url)
+	if not success then return false end
+
+	return urlinfo
+end
+
+function SERVICE:Match( url )
+
+	local info = parseURL(url)
+	if info then
+
+		-- Reject archive.org URLs with /details/ path to prevent conflicts
+		if info.host and info.host:match("archive%.org") and
+		   		info.path and info.path:match("^/details/(.+)$") then
+			return false
+		end
+
+		-- check supported file extensions
+		local ext = info["file"] and info["file"]["ext"] or false
+		if ext then
+			for _, ext2 in pairs(self.FileExtensions) do
+				if ext == ext2 then
+					return true
+				end
 			end
 		end
 	end
+
 
 	return false
 end

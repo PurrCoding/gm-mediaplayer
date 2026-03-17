5d38eef04b8645e2e2d103389d96d54a846f543d
Shadowsun™ <12494967+CattoGamer@users.noreply.github.com>
2026-03-17 18:04:17 +0100

    Revert "Update init.lua"
    This reverts commit 0cf132f8575df17a4dd82fd857702b3ce815e981.

--- lua/mediaplayer/players/spatial/init.lua
+++ lua/mediaplayer/players/spatial/init.lua
@@ -3,11 +3,44 @@ include "shared.lua"
 
 DEFINE_BASECLASS( "mp_base" )
 
+local HearRadiusCvar = CreateConVar( "mediaplayer_spatial_hear_radius", 1800, {
+	FCVAR_ARCHIVE,
+	FCVAR_NOTIFY,
+	FCVAR_REPLICATED
+}, "How far spatial media anchors can be heard before listeners are removed." )
+
+local MaxRepeatCountCvar = CreateConVar( "mediaplayer_spatial_max_repeat_count", 16, {
+	FCVAR_ARCHIVE,
+	FCVAR_NOTIFY,
+	FCVAR_REPLICATED
+}, "Maximum number of additional finite repeats allowed for spatial media anchors." )
+
 function MEDIAPLAYER:NetWriteUpdate()
 	local entIndex = IsValid(self.Entity) and self.Entity:EntIndex() or 0
 	net.WriteUInt(entIndex, 16)
 end
 
+function MEDIAPLAYER:UpdateListeners()
+	local ent = self:GetEntity()
+
+	if not IsValid(ent) then
+		self:SetListeners({})
+		return
+	end
+
+	local radius = math.max( HearRadiusCvar:GetFloat(), 1 )
+	local radiusSqr = radius * radius
+	local pos = ent:GetPos()
+	local listeners = {}
+
+	for _, ply in ipairs(player.GetHumans()) do
+		if IsValid(ply) and ply:GetPos():DistToSqr(pos) <= radiusSqr then
+			table.insert( listeners, ply )
+		end
+	end
+
+	self:SetListeners( listeners )
+end
 
 function MEDIAPLAYER:CanPlayerRequestMedia( ply, media )
 	if not self:IsPlayerPrivileged(ply) then
